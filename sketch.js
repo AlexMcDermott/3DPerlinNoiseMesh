d504e377ac35fe13a3a77c54d5433f20e65ce8c3
AlexMcDermott <alexjmcd2202@gmail.com>
2018-03-12 19:26:44 +1100

    Basic implementation

--- sketch.js
+++ sketch.js
@@ -1,8 +1,21 @@
+scl = 5;
+noiseScl = 0.05;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
+  background(0);
+  cols = floor(windowWidth / scl);
+  rows = floor(windowHeight / scl);
 }
 
 function draw() {
-
+  for (let y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+      noiseVal = noise(x*noiseScl, y*noiseScl);
+      fill(noiseVal*255);
+      noStroke();
+      rect(x*scl, y*scl, scl, scl);
+    }
+  }
+  noLoop();
 }
