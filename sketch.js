22cd3021859e722bc17a33e71d97108800d26dbf
AlexMcDermott <alexjmcd2202@gmail.com>
2018-03-15 11:40:20 +1100

    3D mesh generation

--- sketch.js
+++ sketch.js
@@ -1,27 +1,26 @@
-let scl = 10;
-let noiseScl = 0.05;
-let mouseScl = 0.025;
-let xoff;
-let yoff;
+let scl = 20;
+let noiseScl = 0.0125;
+let amp = 4;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   background(0);
-  cols = floor(windowWidth / scl);
-  rows = floor(windowHeight / scl);
-  xoff = mouseX*mouseScl;
-  yoff = mouseY*mouseScl;
+  stroke(255);
+  noFill();
 }
 
 function draw() {
-  for (let y = 0; y < rows; y++) {
-    for (let x = 0; x < cols; x++) {
-      noiseVal = noise((x*noiseScl)+xoff, (y*noiseScl)+yoff);
-      fill(noiseVal*255);
-      noStroke();
-      rect(x*scl, y*scl, scl, scl);
+  translate(-width/2, height/2, -50);
+  rotateX(-(2*PI)/3);
+  for (let y = 0; y <= height; y += scl) {
+    beginShape(QUAD_STRIP);
+    for (let x = 0; x <= width; x += scl) {
+      noiseVal1 = noise(x*noiseScl, y*noiseScl);
+      noiseVal2 = noise(x*noiseScl, (y+scl)*noiseScl);
+      vertex(x, y, noiseVal1*scl*amp);
+      vertex(x, y+scl, noiseVal2*scl*amp);
     }
+    endShape();
   }
-  xoff = mouseX*mouseScl;
-  yoff = mouseY*mouseScl;
+  noLoop();
 }
