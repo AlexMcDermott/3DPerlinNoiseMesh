5800d76f5ba9d532dd190c63becc610894b3fb77
AlexMcDermott <alexjmcd2202@gmail.com>
2018-07-19 19:56:32 +1000

    Applied linting

--- sketch.js
+++ sketch.js
@@ -10,17 +10,19 @@ function setup() {
 }
 
 function draw() {
-  translate(-width/2, height/2, -50);
-  rotateX(-(2*PI)/3);
+  translate(-width / 2, height / 2, -50);
+  rotateX(-(2 * PI) / 3);
   for (let y = 0; y <= height; y += scl) {
     beginShape(QUAD_STRIP);
     for (let x = 0; x <= width; x += scl) {
-      noiseVal1 = noise(x*noiseScl, y*noiseScl);
-      noiseVal2 = noise(x*noiseScl, (y+scl)*noiseScl);
-      vertex(x, y, noiseVal1*scl*amp);
-      vertex(x, y+scl, noiseVal2*scl*amp);
+      noiseVal1 = noise(x * noiseScl, y * noiseScl);
+      noiseVal2 = noise(x * noiseScl, (y + scl) * noiseScl);
+      vertex(x, y, noiseVal1 * scl * amp);
+      vertex(x, y + scl, noiseVal2 * scl * amp);
     }
+
     endShape();
   }
+
   noLoop();
 }
